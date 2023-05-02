d830c869aa5dd98671b1ac62fd0527e5a8028173
Shoaib <Shoaibk909@gmail.com>
2023-05-02 16:16:43 +0530

    table-design

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,28 +1,50 @@
-import React from 'react'
+import React from "react";
 
 const Table = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <table>
         <thead>
-            <tr>
+          <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Email Id</th>
             <th>DOJ</th>
-            </tr>
+            <th>Edit</th>
+          </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>apple</td>
-                <td>xyz@gmail.com</td>
-                <td>24-02-2023</td>
-            </tr>
+          <tr>
+            <td>1</td>
+            <td>apple</td>
+            <td>xyz@gmail.com</td>
+            <td>24-02-2023</td>
+            <td className="edit-btn">
+              <button className="btn">EDIT</button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>mango</td>
+            <td>xyz@gmail.com</td>
+            <td>24-02-2023</td>
+            <td className="edit-btn">
+              <button className="btn">EDIT</button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>grapes</td>
+            <td>xyz@gmail.com</td>
+            <td>24-02-2023</td>
+            <td className="edit-btn">
+              <button className="btn">EDIT</button>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
